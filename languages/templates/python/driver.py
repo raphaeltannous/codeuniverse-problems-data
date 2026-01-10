8a64ad5f60325f28dd22d918dcfe359bc01bd1cb
Raphael Tannous <raphaeltannous@proton.me>
2026-01-10 21:43:23 +0200

    Updating python

--- languages/templates/python/driver.py
+++ languages/templates/python/driver.py
@@ -1,6 +1,6 @@
 import json
 
 
-def load_tests(testcases_file: "testcases.json"):
+def load_testcases(testcases_file: "testcases.json"):
     with open(testcases_file, "r", encoding="utf-8") as file:
         return json.load(file)
