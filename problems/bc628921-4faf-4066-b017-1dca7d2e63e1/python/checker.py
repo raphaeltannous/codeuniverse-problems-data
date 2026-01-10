e12b86fce7d271aa5c3f62150c7dd55c61c6900e
Raphael Tannous <raphaeltannous@proton.me>
2026-01-10 20:34:59 +0200

    Updating problem

--- problems/bc628921-4faf-4066-b017-1dca7d2e63e1/python/checker.py
+++ problems/bc628921-4faf-4066-b017-1dca7d2e63e1/python/checker.py
@@ -1,31 +1,36 @@
 def run_checker(testcases, solution):
-    # result:
-    #  - id
-    #  - input
-    #  - expected
-    #  - got
-    #  - exception
-    #  - isPassed
-    result = {
+    # results:
+    # - exception
+    # - isPassed
+    # - failedTestcases:
+    #      - id
+    #      - input
+    #      - expected
+    #      - got
+    results = {
         "isPassed": True,
+        "failedTestcases": [],
     }
 
     for testcase in testcases:
         input = testcase["input"]
         expected = testcase["expected"]
+        result = {}
 
         try:
             got = solution.toLower(input)
+
             if got != expected:
-                result.id = testcase["id"]
-                result.input = input
-                result.expected = expected
-                result.got = got
-                result.isPassed = False
-                break
+                result["id"] = testcase["id"]
+                result["input"] = input
+                result["expected"] = expected
+                result["got"] = got
+
+                results["isPassed"] = False
+                results["failedTestcases"].append(result)
         except Exception as e:
-            result.exception = e
-            result.isPassed = False
+            results["exception"] = f"{e}"
+            results["isPassed"] = False
             break
 
-    return result
+    return results
