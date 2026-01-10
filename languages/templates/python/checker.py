8bd620bd3c385972d6b32b8cd2890e6ad20e3c27
Raphael Tannous <raphaeltannous@proton.me>
2026-01-10 20:35:18 +0200

    Updating python checker template

--- languages/templates/python/checker.py
+++ languages/templates/python/checker.py
@@ -1,33 +1,37 @@
 def run_checker(testcases, solution):
-    # result:
-    #  - id
-    #  - input
-    #  - expected
-    #  - got
-    #  - exception
-    #  - isPassed
-    result = {
+    # results:
+    # - exception
+    # - isPassed
+    # - failedTestcases:
+    #      - id
+    #      - input
+    #      - expected
+    #      - got
+    results = {
         "isPassed": True,
+        "failedTestcases": [],
     }
 
     for testcase in testcases:
         input = testcase["input"]
         expected = testcase["expected"]
+        result = {}
 
         try:
             # SET FUNCTION
             # got = solution.FUNCTION(input)
 
             if got != expected:
-                result.id = testcase["id"]
-                result.input = input
-                result.expected = expected
-                result.got = got
-                result.isPassed = False
-                break
+                result["id"] = testcase["id"]
+                result["input"] = input
+                result["expected"] = expected
+                result["got"] = got
+
+                results["isPassed"] = False
+                results["failedTestcases"].append(result)
         except Exception as e:
-            result.exception = e
-            result.isPassed = False
+            results["exception"] = e
+            results["isPassed"] = False
             break
 
-    return result
+    return results
