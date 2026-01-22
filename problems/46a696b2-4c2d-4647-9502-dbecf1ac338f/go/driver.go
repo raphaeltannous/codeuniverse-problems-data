ee978f449587de96ad136acd5789fb4afbc8d2b8
Raphael Tannous <raphaeltannous@proton.me>
2026-01-22 09:44:37 +0200

    Updating linkedlist testcases

--- problems/46a696b2-4c2d-4647-9502-dbecf1ac338f/go/driver.go
+++ problems/46a696b2-4c2d-4647-9502-dbecf1ac338f/go/driver.go
@@ -7,39 +7,10 @@ import (
 )
 
 type Testcase struct {
-	Id       int  `json:"id"`
-	Input    []int  `json:"input"`
-	Expected []int  `json:"expected"`
-	IsPublic bool `json:"isPublic"`
-}
-
-type Node struct {
-	Val int
-	Next *Node
-}
-
-func sliceToLinkedList(nums []int) *Node {
-	if len(nums) == 0 {
-		return nil
-	}
-
-	dummy := &Node{}
-	curr := dummy
-
-	for _, num := range nums {
-		curr.Next = &Node{Val: num}
-		curr = curr.Next
-	}
-
-	return dummy.Next
-}
-
-func linkedListToSlice(head *Node) []int {
-	nums := make([]int, 0, 50)
-	for e := head; e != nil; e = e.Next {
-		nums = append(nums, e.Val)
-	}
-	return nums
+	Id       int   `json:"id"`
+	Input    []int `json:"input"`
+	Expected []int `json:"expected"`
+	IsPublic bool  `json:"isPublic"`
 }
 
 func loadTestcases(filename string) []*Testcase {
